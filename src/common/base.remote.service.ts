10fade08d5c3fb0795ec163cc5539a11cd67fab4
Martin Liu <hflh1989@gmail.com>
2017-06-21 14:25:22 -0700

    refine code

--- src/common/base.remote.service.ts
+++ src/common/base.remote.service.ts
@@ -41,12 +41,7 @@ export class BaseRemoteService {
 
   doQueryWithCache(method, param, canceler = null, timeout = 300){
     return Observable.fromPromise(this.getWithCache(method, param, () => {
-      if (canceler && canceler.promise) {
-        let config = {timeout: canceler.promise};
-        return this.rest.one(method).withHttpConfig(config).get(param);
-      } else {
-        return this.rest.one(method).get(param);
-      }
+      return this.doQuery(method, param, canceler).toPromise();
     }, timeout));
   }
 
