13aa7d515595ede1fde52873852e6a7d505ef6dd
Martin Liu <hflh1989@gmail.com>
2017-08-02 11:38:21 -0700

    refine code

--- src/config/config.dist.ts
+++ src/config/config.dist.ts
@@ -1,5 +1,9 @@
 // `config.dist.ts` is the template for local `config.ts`
 
 export const Config = {
-  production: false
+  production: false,
+  uri: {
+    api: "http://localhost:1337/"
+  },
+
 };
