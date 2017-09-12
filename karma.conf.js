9fc84a49b22cc0d1728f55db7e6c365b979b1bb4
Martin Liu <hflh1989@gmail.com>
2017-09-12 16:50:33 -0700

    refine code

--- karma.conf.js
+++ karma.conf.js
@@ -49,6 +49,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      headless: {
+        base: 'ChromiumHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
